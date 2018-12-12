88f17a2e4261134f60547179c604ecf721f2d98c
McCubo <cubiascaceres@gmail.com>
2018-12-11 18:34:44 -0600

    Adding Sessions controller and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 Rails.application.routes.draw do
   root :to => "articles#index"
   resources :articles do
-    resources :comments
+    resources :comments, :only => [:destroy, :show, :new, :create]
   end
   resources :users
+  resource :session, :only => [:new, :create, :destroy]
+  get '/login' => "sessions#new", :as => "login"
+  get '/logout' => "sessions#destroy", :as => "logout"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get '/teams/search/:query' => 'mccubo#search', :as => 'mccubo'
 end
