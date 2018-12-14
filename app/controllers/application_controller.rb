678a89dc9e7251ba0ba928254be422ddc0bd14cf
McCubo <cubiascaceres@gmail.com>
2018-12-14 11:43:19 -0600

    adding session controller and helper methods

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,24 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  
+  protected
+    def current_user
+      return unless session[:user_id]
+      @current_user ||= User.find_by_id(session[:user_id])
+    end
+       
+    def authenticate
+      logged_in? || access_denied
+    end
+    
+    def logged_in?
+      current_user.is_a? User
+    end
+            
+    def access_denied
+      redirect_to login_path, notice: "Please log in to continue" and return false
+    end
+
+    # Make method available in template as a helper
+    helper_method [:current_user, :logged_in?]    
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,5 @@
 module ApplicationHelper
+  def submit_cancel(form, name="Cancel")
+    form.submit + " " + link_to(name, 'javascript:history.go(-1);', {:class => 'cancel'})
+  end
 end
